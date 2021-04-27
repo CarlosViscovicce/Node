a4468eba3888f2d2e15a8facf0271df46f715b03
Carlos Viscovicce <carloseduviscovicce@hotmail.com>
2021-04-26 22:30:39 -0300

    Primeiro Commit Atividade 1

---
+++ src/atividade1.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import './atividade1.css';
+
+
+function Atividade1() {
+
+  const [user, setUser] = useState("")
+  const [phone, setPhone] = useState("")
+
+  return (
+    <div className="atividade1">
+      <form>
+        <div>
+          <p>
+            Digite seu nome:
+          </p>
+          <input
+            value={user}
+            onChange={e => setUser(e.target.value)}
+            placeholder="Usuario"
+          />
+        </div>
+        <div>
+          <p>
+            Digite o telefone:
+          </p>
+          <input
+            value={phone}
+            onChange={e => setPhone(e.target.value)}
+            placeholder="14 2108-2084"
+          />
+        </div>
+      </form>
+      <div className="Resultado_Atividade">
+        <div className="Resultados">
+          <p>{user}</p>
+          <p>{phone}</p>
+        </div>
+      </div>
+
+     
+
+    </div>
+  );
+}
+
+export default Atividade1;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Atividade1 from './atividade1';
+ReactDOM.render(
+  <Atividade1/>
+,
+  document.getElementById('root')
+);
